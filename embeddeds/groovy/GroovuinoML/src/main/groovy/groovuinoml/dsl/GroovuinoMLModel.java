03c55a2327d9346b83c807437fccc358c4a1eac9
Clément Forneris <clement.forneris@gmail.com>
2016-01-16 20:23:19 +0100

    Loulou Bug

--- embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/dsl/GroovuinoMLModel.java
+++ embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/dsl/GroovuinoMLModel.java
@@ -1,5 +1,6 @@
 package main.groovy.groovuinoml.dsl;
 
+
 import java.util.*;
 
 import groovy.lang.Binding;
@@ -9,21 +10,30 @@ import io.github.mosser.arduinoml.kernel.behavioral.State;
 import io.github.mosser.arduinoml.kernel.behavioral.Transition;
 import io.github.mosser.arduinoml.kernel.generator.ToWiring;
 import io.github.mosser.arduinoml.kernel.generator.Visitor;
+import io.github.mosser.arduinoml.kernel.lib.Library;
+import io.github.mosser.arduinoml.kernel.lib.LibraryUse;
+import io.github.mosser.arduinoml.kernel.lib.Measure;
+import io.github.mosser.arduinoml.kernel.lib.MeasureUse;
 import io.github.mosser.arduinoml.kernel.structural.Actuator;
 import io.github.mosser.arduinoml.kernel.structural.Brick;
 import io.github.mosser.arduinoml.kernel.structural.SIGNAL;
 import io.github.mosser.arduinoml.kernel.structural.Sensor;
 
+
 public class GroovuinoMLModel {
 	private List<Brick> bricks;
 	private List<State> states;
 	private State initialState;
+	private List <LibraryUse> usedLibraries;
+    private List <MeasureUse> usedMeasure;
 	
 	private Binding binding;
 	
 	public GroovuinoMLModel(Binding binding) {
 		this.bricks = new ArrayList<Brick>();
 		this.states = new ArrayList<State>();
+		this.usedLibraries = new ArrayList<LibraryUse>();
+        this.usedMeasure = new ArrayList<MeasureUse>();
 		this.binding = binding;
 	}
 	
@@ -59,10 +69,22 @@ public class GroovuinoMLModel {
 		//transition.setValue(value);
 		from.setTransition(transition);
 	}
-	
-	public void setInitialState(State state) {
-		this.initialState = state;
+
+	public void createLibraryUse(Library library,Map <String, String> argsValues){
+		LibraryUse libraryUse = new LibraryUse();
+		libraryUse.setLibrary(library);
+		libraryUse.setArgsValues(argsValues);
+        this.usedLibraries.add(libraryUse);
 	}
+
+    public void createMeasureUse(LibraryUse libraryUse, Measure measure,Map <String, String> argsValues){
+        MeasureUse measureUse = new MeasureUse();
+        measureUse.setLibraryUse(libraryUse);
+        measureUse.setMeasure(measure);
+        measureUse.setArgsValues(argsValues);
+        this.usedMeasure.add(measureUse);
+    }
+
 	
 	@SuppressWarnings("rawtypes")
 	public Object generateCode(String appName) {
