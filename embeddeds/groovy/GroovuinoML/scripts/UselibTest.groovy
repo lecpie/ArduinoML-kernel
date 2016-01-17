8047228e660652b571c94060e089bdf9bca072a4
Louis Pennachio <pennachiolouis@gmail.com>
2016-01-17 17:40:00 +0100

    adding a script for uselib testing

---
+++ embeddeds/groovy/GroovuinoML/scripts/UselibTest.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by Louis on 17/01/2016.
+ */
+
+//uselib "DHT" like "temperature_unit" "celsius" and "param2" "param_value2"
+uselib "DHT" like "temperature_unit" "celsius"
+    sensor "humidity_1" pin 6
+    sensor "temperature_1" pin 2
