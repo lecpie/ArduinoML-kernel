5ed6595646bee4bceaa3effade9e6aae54c5679a
Clément Forneris <clement.forneris@gmail.com>
2016-01-26 19:08:00 +0100

    ta mere

--- embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/morse_dsl/MorseMLBaseScript.groovy
+++ embeddeds/groovy/GroovuinoML/src/main/groovy/groovuinoml/morse_dsl/MorseMLBaseScript.groovy
@@ -1,5 +1,12 @@
 package main.groovy.groovuinoml.morse_dsl
 
+import io.github.mosser.arduinoml.kernel.App
+import io.github.mosser.arduinoml.kernel.behavioral.Action
+import io.github.mosser.arduinoml.kernel.behavioral.DigitalExpression
+import io.github.mosser.arduinoml.kernel.behavioral.State
+import io.github.mosser.arduinoml.kernel.language.Actionable
+import io.github.mosser.arduinoml.kernel.structural.PinnedActuator
+
 
 /**
  * Created by fofo on 26/01/16.
@@ -8,11 +15,31 @@ abstract class MorseMLBaseScript extends Script {
 
 
     def morse(String message){
+
+        App app = new App();
+
+        State state = new State()
+        List <Actionable> actions = new ArrayList<>();
+        state.setName("morse")
+        state.setActions(actions)
+
+        PinnedActuator actuator = app.getBricks().get(0)
+
+        Action on = new Action()
+        on.setActuator(actuator)
+        on.setSignalExpression(new DigitalExpression(true))
+
+        Action off = new Action()
+        off.setActuator(actuator)
+        off.setSignalExpression(new DigitalExpression(false))
+
+        Sleep sleep = new Sleep()
+
         message = message.toLowerCase()
         ((MorseMLBinding)this.getBinding()).getMorseMLModel().encodeMessage(message)
         for( Morse_Type type :((MorseMLBinding)this.getBinding()).getMorseMLModel().getMorse_answer()){
                 if(type.equals(Morse_Type.LONG_MORSE)){
-                    print("-")
+                    actions.add()
                 }
                 else if(type.equals(Morse_Type.SHORT_MORSE)){
                     print(".")
